61e812b5060b74b26a0edf715bc5d657624b6b56
ozcancore <77274524+ozcancore@users.noreply.github.com>
2021-08-21 21:38:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var rpc = require("discord-rpc")
+const client = new rpc.Client({ transport: 'ipc' })
+client.on('ready', () => {
+client.request('SET_ACTIVITY', {
+pid: process.pid,
+activity : {
+details : "Recep AB",
+assets : {
+large_image : "recep",
+large_text : "Recep in the house",
+small_image : "nene",
+small_text : "Ekinler baş vermeden kör buzağı topallamazmış"
+
+},
+buttons : [{label : "31" , url : "https://www.ozcan.info"},{label : "Mutlaka İzle",url : "https://www.youtube.com/watch?v=Ei3HaqPCIWA"}] //kendinize göre yazın
+}
+})
+})
+client.login({ clientId : "818627795596869654" }).catch(console.error);
+
+
+
+
+client.on('ready', () => {
+    console.log("Önden giriş yapıldı");
+
+  });               //ozcancore 
